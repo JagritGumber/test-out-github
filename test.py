e96e374be71c10d60824796089d468bb79380c5a
JagritGumber <gumberjagrit@gmail.com>
2026-06-30 23:26:37 +0530

    refactor: enhance logging in test.py

--- test.py
+++ test.py
@@ -1,9 +1,16 @@
+from asyncio import log
+
+
 print("testing stuff")
 
 print("Someone will not die today")
 
+log.info("I am testing stuff")
 
 print("I am testing stuff")
 
+print("I am testing stuff again")
+
+log.info("I am testing stuff again")
 
 print("I am testing stuff again")
